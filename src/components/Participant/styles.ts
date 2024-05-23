604a750c4f68bbf6d7c2d4c6583756cfbb1e3df4
Matheus Amon <amonmatheus757@gmail.com>
2024-05-23 20:29:49 -0300

    feat: add participant component

---
+++ src/components/Participant/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: '#1F1E25',
+        borderRadius: 5, 
+        flexDirection: 'row',
+        alignItems: 'center', 
+        marginBottom: 8,
+        paddingLeft: 16,
+    },
+    name: {
+        color: '#FFF',
+        fontSize: 16,
+        flex: 1,
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#FDB29B',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold',
+    }
+})

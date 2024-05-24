525aa2eaee0174b43e472e095cb5f4512e21e5e8
Matheus Amon <amonmatheus757@gmail.com>
2024-05-23 21:09:22 -0300

    feat: add initial participants list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,49 @@
-import { Text, View } from "react-native"
+import { ScrollView, FlatList,Text, View } from "react-native"
 import { styles } from "./styles"
 import { Form } from "../../components/Form"
 import { Participant } from "../../components/Participant"
 
 export function Home () { 
+  const participants = [
+    'Matheus',
+    'João',
+    'Maria',
+    'Pedro',
+    'Lucas',
+    'Ana',
+    'Carla',
+    'Bruna',
+    'Gabriel',
+    'Luciana',
+    'Vitoria',
+    'Isabela',
+    'Lara',
+    'Mariana',
+    'Bruno',
+    'Vitor',
+  ]
+
+  const handleParticipantRemover = (name: string) => {
+    console.log(`Participante ${name} removido!`)
+  }
+
   return (
     <View style={styles.view}>
       <Text style={styles.title}>React Native</Text>
       <Text style={styles.subTitle}>Quinta, 23 de Maio de 2024</Text>
       <Form />
-    <Participant name="Matheus" />
-      <Participant name="Lucas" />
-      <Participant name="Pedro" />
+      <FlatList 
+        data={participants} 
+        keyExtractor={(item) => item} 
+        renderItem={({ item, index }) => (
+          <Participant 
+            key={index}
+            name={item} 
+            onRemove={() => handleParticipantRemover(item)} 
+          />
+        )} 
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   )
 }
